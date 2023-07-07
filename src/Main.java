fcd76dcc8c011e0c373fe4ed4f01703bed1e6896
张旭波 <2066382553@qq.com>
2023-07-07 20:39:59 +0800

    test

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @className: Main
+ * @author: zxb
+ * @create: 2023-07-07 20:38
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+    }
+}
+
